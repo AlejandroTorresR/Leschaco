893ce2bee1337295adde2454a918c09918a12505
AlejandroTorresR <alejandrotorresroa@gmail.com>
2017-01-28 21:52:26 -0600

    Leschaco app

---
+++ src/app/app.module.ts
@@ -0,0 +1,74 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonCalendar } from '@ionic2-extra/calendar';
+
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+import { ConferenceApp } from './app.component';
+
+import { CalendarPage } from '../pages/calendar/calendar';
+import { AboutPage } from '../pages/about/about';
+import { PopoverPage } from '../pages/about-popover/about-popover';
+import { AccountPage } from '../pages/account/account';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { SchedulePage } from '../pages/schedule/schedule';
+import { ScheduleFilterPage } from '../pages/schedule-filter/schedule-filter';
+import { SessionDetailPage } from '../pages/session-detail/session-detail';
+import { SignupPage } from '../pages/signup/signup';
+import { SpeakerDetailPage } from '../pages/speaker-detail/speaker-detail';
+import { SpeakerListPage } from '../pages/speaker-list/speaker-list';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { SupportPage } from '../pages/support/support';
+
+import { ConferenceData } from '../providers/conference-data';
+import { UserData } from '../providers/user-data';
+
+
+@NgModule({
+  declarations: [
+    IonCalendar,
+    ConferenceApp,
+    AboutPage,
+    CalendarPage,
+    AccountPage,
+    LoginPage,
+    MapPage,
+    PopoverPage,
+    SchedulePage,
+    ScheduleFilterPage,
+    SessionDetailPage,
+    SignupPage,
+    SpeakerDetailPage,
+    SpeakerListPage,
+    TabsPage,
+    TutorialPage,
+    SupportPage
+  ],
+  imports: [
+    IonicModule.forRoot(ConferenceApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    ConferenceApp,
+    AboutPage,
+    CalendarPage,
+    AccountPage,
+    LoginPage,
+    MapPage,
+    PopoverPage,
+    SchedulePage,
+    ScheduleFilterPage,
+    SessionDetailPage,
+    SignupPage,
+    SpeakerDetailPage,
+    SpeakerListPage,
+    TabsPage,
+    TutorialPage,
+    SupportPage
+  ],
+  providers: [ConferenceData, UserData, Storage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
